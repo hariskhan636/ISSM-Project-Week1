4a8bdb07f8df825bc8f7bc9210fee4c44e473991
hariskhancg <haris.khan@codegenio.com>
2024-02-06 17:55:11 +0500

    refactor: simplify code using spread operator

--- issm-project-week1/src/App.js
+++ issm-project-week1/src/App.js
@@ -4,25 +4,13 @@ import Card from "./components/Card";
 import cardData from "./cardData";
 function App() {
   const cardElements = cardData.map((card) => {
-    return (
-      <Card
-        key={card.id}
-        img={card.img}
-        rating={card.rating}
-        reviewCount={card.reviewCount}
-        country={card.country}
-        title={card.title}
-        price={card.price}
-        openSpots={card.openSpots}
-        location={card.location}
-      />
-    );
+    return <Card key={card.id} {...card} />;
   });
   return (
     <div>
       <Navbar />
-      <section className="card-list">{cardElements}</section>
       <Hero />
+      <section className="card-list">{cardElements}</section>
     </div>
   );
 }

43bd7431602bbf8afae6d6b9a87775445e146d0b
hariskhancg <haris.khan@codegenio.com>
2024-02-06 17:14:43 +0500

    refactor: display cards through data from constant file and array.map

--- issm-project-week1/src/App.js
+++ issm-project-week1/src/App.js
@@ -1,10 +1,21 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
-import person1 from "./images/person1.png";
-import wedding from "./images/wedding-photography.png";
-import bike from "./images/mountain-bike.png";
+import cardData from "./cardData";
 function App() {
+  const cardElements = cardData.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        img={card.img}
+        rating={card.rating}
+        reviewCount={card.reviewCount}
+        country={card.country}
+        title={card.title}
+        price={card.price}
+      />
+    );
+  });
   return (
     <div>
       <Navbar />
@@ -14,30 +25,7 @@ function App() {
           flexdirection: "row",
         }}
       >
-        <Card
-          img={person1}
-          rating={5.0}
-          reviewCount={6}
-          country="USA"
-          title="Life Lessons with Katie Zaferes"
-          price="$136"
-        />
-        <Card
-          img={wedding}
-          rating={5.0}
-          reviewCount={30}
-          country="Pakistan"
-          title="Learn wedding photography"
-          price="$125"
-        />
-        <Card
-          img={bike}
-          rating={4.8}
-          reviewCount={2}
-          country="UK"
-          title="Group Mountain Biking"
-          price="$50"
-        />
+        {cardElements}
       </div>
 
       <Hero />
---
+++ issm-project-week1/src/cardData.js
@@ -0,0 +1,33 @@
+import person1 from "./images/person1.png";
+import wedding from "./images/wedding-photography.png";
+import bike from "./images/mountain-bike.png";
+
+export default [
+  {
+    id: 1,
+    img: person1,
+    rating: 5.0,
+    reviewCount: 6,
+    country: "USA",
+    title: "Life Lessons with Katie Zaferes",
+    price: "$136",
+  },
+  {
+    id: 2,
+    img: wedding,
+    rating: 5.0,
+    reviewCount: 30,
+    country: "Pakistan",
+    title: "Learn weddings photography",
+    price: "$125",
+  },
+  {
+    id: 3,
+    img: bike,
+    rating: 4.8,
+    reviewCount: 2,
+    country: "UK",
+    title: "Group Mountain Biking",
+    price: "$50",
+  },
+];

b75c8ac33c2d1e7972c58e715d46ede8b78759d0
hariskhancg <haris.khan@codegenio.com>
2024-02-06 15:57:25 +0500

    feat: add card component

--- issm-project-week1/src/App.js
+++ issm-project-week1/src/App.js
@@ -1,9 +1,45 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import person1 from "./images/person1.png";
+import wedding from "./images/wedding-photography.png";
+import bike from "./images/mountain-bike.png";
 function App() {
   return (
     <div>
       <Navbar />
+      <div
+        style={{
+          display: "flex",
+          flexdirection: "row",
+        }}
+      >
+        <Card
+          img={person1}
+          rating={5.0}
+          reviewCount={6}
+          country="USA"
+          title="Life Lessons with Katie Zaferes"
+          price="$136"
+        />
+        <Card
+          img={wedding}
+          rating={5.0}
+          reviewCount={30}
+          country="Pakistan"
+          title="Learn wedding photography"
+          price="$125"
+        />
+        <Card
+          img={bike}
+          rating={4.8}
+          reviewCount={2}
+          country="UK"
+          title="Group Mountain Biking"
+          price="$50"
+        />
+      </div>
+
       <Hero />
     </div>
   );

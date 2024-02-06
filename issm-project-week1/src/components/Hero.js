81f88145fb330cce568e3f976c0df51e1851a375
hariskhancg <haris.khan@codegenio.com>
2024-02-06 14:06:02 +0500

    feat: add hero component

---
+++ issm-project-week1/src/components/Hero.js
@@ -0,0 +1,15 @@
+import photogrid from "../images/photo-grid.png";
+const Hero = () => {
+  return (
+    <section className="hero">
+      <img src={photogrid} alt="hero" className="hero-image" />
+      <h1 className="hero-header">Online Experiences</h1>
+      <p className="hero-text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;

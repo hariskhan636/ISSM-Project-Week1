0dfda9d5803c26d979bb3b4a6e2a4b791d81f612
hariskhancg <haris.khan@codegenio.com>
2024-02-11 15:54:08 +0500

    refactor: navbar move to layout

--- issm-project-week1/src/App.js
+++ issm-project-week1/src/App.js
@@ -1,5 +1,5 @@
 import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+import Navbar from "./layout/Navbar/Navbar";
 import Card from "./components/Card";
 import cardData from "./constants/cardData";
 function App() {
